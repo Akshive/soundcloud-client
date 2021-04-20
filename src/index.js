7bd1e91cce8502be217d902f3af255c7e4ad37d3
Akshive <pandeyakshive97@gmail.com>
2021-04-20 22:19:28 +0530

    soundcloud-client

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const tracks = [{ title: "Some track" }, { title: "Some other track" }];
+
+ReactDOM.render(
+  <div>
+    {tracks.map((track) => (
+      <div className="track">{track.title}</div>
+    ))}
+  </div>,
+  document.getElementById("app")
+);
+
+module.hot.accept();
