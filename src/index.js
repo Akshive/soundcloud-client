b556fb1314683443ac8a7db4cd43437f753f5dd6
Akshive <pandeyakshive97@gmail.com>
2021-04-22 12:07:29 +0530

    adding store

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from "react";
+import { Provider } from "react-redux";
 import ReactDOM from "react-dom";
+import configureStore from "./stores/configureStore";
+import * as actions from "./actions";
+import Stream from "./components/Stream";
 
 const tracks = [{ title: "Some track" }, { title: "Some other track" }];
 
+const store = configureStore();
+store.dispatch(actions.setTracks(tracks));
+
 ReactDOM.render(
-  <div>
-    {tracks.map((track) => (
-      <div className="track">{track.title}</div>
-    ))}
-  </div>,
+  <Provider store={store}>
+    <Stream />
+  </Provider>,
   document.getElementById("app")
 );
 
